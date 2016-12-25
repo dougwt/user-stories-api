f0df74a915a8bf8ec93a6635fe52cceb9a76ddd1
Doug Tyler <dougwt@gmail.com>
2016-12-24 19:42:10 -0800

    Add initial API tests

--- server/index.js
+++ server/index.js
@@ -2,12 +2,18 @@ import express from 'express'
 import http from 'http'
 import bodyParser from 'body-parser'
 import morgan from 'morgan'
+import mongoose from 'mongoose'
+import router from './router'
 
 let app = express()
+
+// DB setup
+// mongoose.connect('mongodb://localhost:test')
 
 // App setup
 app.use(morgan('combined'))
 app.use(bodyParser.json({ type: '*/*' }))
+router(app)
 
 // Server setup
 export const port = process.env.API_PORT || 3090
--- server/test/index_test.js
+++ server/test/index_test.js
@@ -21,16 +21,45 @@ describe('Users', function(done) {
         res.should.have.status(200)
         res.should.be.json
         res.body.should.be.a('array')
+        res.body.length.should.equal(4)
+        res.body[0].should.have.property('id');
+        res.body[0].should.have.property('email')
+        res.body[0].should.have.property('name')
+        res.body[0].should.have.property('creation_date')
+        res.body[0].id.should.equal(1)
+        res.body[0].email.should.equal('doug@example.com')
+        res.body[0].name.should.equal('Doug')
+        res.body[0].creation_date.should.equal('1982-07-07T07:00:00.000Z')
+        res.body[1].name.should.equal('Ada Lovelace')
+        res.body[2].name.should.equal('Grace Hopper')
+        res.body[3].name.should.equal('Margaret Hamilton')
         done()
       })
   })
   it('should list a SINGLE user on /users/<id> GET', function(done) {
     chai.request(server)
-      .get('/users/id')
+      .get('/users/2')
       .end(function(err, res){
         res.should.have.status(200)
         res.should.be.json
-        res.body.should.be.a('array')
+        res.body.should.be.a('object')
+        res.body.should.have.property('id');
+        res.body.should.have.property('email')
+        res.body.should.have.property('name')
+        res.body.should.have.property('creation_date')
+        res.body.id.should.equal(2)
+        res.body.email.should.equal('ada@example.com')
+        res.body.name.should.equal('Ada Lovelace')
+        res.body.creation_date.should.equal('1815-12-10T08:00:00.000Z')
+        done()
+      })
+  })
+  it('should return 404 status on invalid /users/<id> GET', function(done) {
+    chai.request(server)
+      .get('/users/100')
+      .end(function(err, res){
+        res.should.have.status(404)
+        res.should.be.json
         done()
       })
   })
