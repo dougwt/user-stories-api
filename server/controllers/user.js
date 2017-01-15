9da5d4d65698b289f810e678117c88d5b8783892
Doug Tyler <dougwt@gmail.com>
2017-01-14 22:30:57 -0800

    Add User controller stubs

---
+++ server/controllers/user.js
@@ -0,0 +1,25 @@
+import User from '../models/user';
+
+module.exports = {
+
+  findById(req, res, next) {
+
+  },
+
+  findAll(req, res, next) {
+
+  },
+
+  create(req, res, next) {
+
+  },
+
+  update(req, res, next) {
+
+  },
+
+  delete(req, res, next) {
+
+  };
+
+};
