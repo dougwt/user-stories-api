a18ecc35d8e95eeef7b832ac356e5a9aaeac54dd
Doug Tyler <dougwt@gmail.com>
2017-01-15 15:16:53 -0800

    Add initial users tests

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,13 +1,23 @@
 import User from '../models/user';
 
+// const formatResponse(text) {
+//   return
+// }
+
 module.exports = {
 
   findById(req, res, next) {
+    const userId = req.params.id;
 
+    User.findById(userId)
+      .then((user) => res.send(user))
+      .catch(next);
   },
 
   findAll(req, res, next) {
-
+    User.find({})
+      .then((users) => res.send(users))
+      .catch(next);
   },
 
   create(req, res, next) {
@@ -15,7 +25,18 @@ module.exports = {
 
     User.create(userProps)
       .then((user) => res.send(user))
-      .catch(next);
+      .catch((err) => {
+        // console.log(err);
+        if (err.errors.email && err.errors.email.name === 'ValidatorError' && err.errors.email.message === 'Path `email` is required.') {
+          res.status(400).send({ error: 'Email field is required.'})
+          next();
+        } else if (err.errors.name && err.errors.name.name === 'ValidatorError' && err.errors.name.message === 'Path `name` is required.') {
+          res.status(400).send({ error: 'Name field is required.'})
+          next();
+        } else {
+          next(err);
+        }
+      });
   },
 
   update(req, res, next) {
--- server/test/app_test.js
+++ server/test/app_test.js
@@ -1,13 +1,16 @@
 const assert = require('assert');
-const request = require('supertest');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+
 const app = require('../app');
 
 describe('Express app', () => {
   it('handles a GET request to /', (done) => {
-    request(app)
+    chai.request(app)
       .get('/')
       .end((err, response) => {
-        assert(response.body.message === 'Connected!');
+        response.body.message.should.equal('Connected!');
         done();
       });
   });
