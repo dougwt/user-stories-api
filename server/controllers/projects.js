5a756e340c5865787d1333e8e7b2ddf22f00ea36
Doug Tyler <dougwt@gmail.com>
2017-01-21 12:25:43 -0800

    Add POST /projects

--- server/controllers/projects.js
+++ server/controllers/projects.js
@@ -17,6 +17,26 @@ module.exports = {
   create(req, res, next) {
     const projectProps = req.body;
 
+    Project.create(projectProps)
+      // TODO: replace hardcoded URI prefix
+      .then((project) => res.location('https://api.mycodebytes.com/v1/projects/'+ project.id).status(201).send(Response.success(project)))
+      .catch((err) => {
+        if (err.errors.name && err.errors.name.name === 'ValidatorError' && err.errors.name.message === 'Path `name` is required.') {
+          res.status(400).send(Response.error('Name is required.'))
+          next();
+        } else if (err.errors.slug && err.errors.slug.name === 'ValidatorError' && err.errors.slug.message === 'Path `slug` is required.') {
+          res.status(400).send(Response.error('Slug is required.'))
+          next();
+        } else if (err.errors.slug && err.errors.slug.name === 'ValidatorError' && err.errors.slug.message.startsWith('Error, expected `slug` to be unique.')) {
+          res.status(409).send(Response.error('Slug is in use.'))
+          next();
+        } else if (err.errors.slug && err.errors.slug.name === 'ValidatorError' && err.errors.slug.message.startsWith('Validator failed for path `slug`')) {
+            res.status(400).send(Response.error('Slug is invalid.'))
+            next();
+        } else {
+          next(err);
+        }
+      });
   },
 
   update(req, res, next) {
--- server/test/controllers/projects_test.js
+++ server/test/controllers/projects_test.js
@@ -7,7 +7,7 @@ const Project = mongoose.model('project');
 
 chai.use(chaiHttp)
 
-describe('Projects API', () => {
+describe.only('Projects API', () => {
 
   //////////////////////////////////////////////////////////
   //  /projects
@@ -53,29 +53,111 @@ describe('Projects API', () => {
   })
 
   describe('POST /projects', () => {
-    xit('creates a new project', () => {
-
+    it('creates a new project', (done) => {
+      Project.count().then(count => {
+        chai.request(app)
+          .post('/projects')
+          .send({ name: 'Test', slug: 'test' })
+          .end((err, res) => {
+            Project.count().then(newCount => {
+              res.should.have.status(201);
+              res.should.be.json;
+              res.headers.should.have.property('location');
+              res.headers.location.startsWith('https://api.mycodebytes.com/v1/projects/');
+              res.body.status.should.equal('success');
+              newCount.should.equal(count + 1);
+              done();
+            });
+          });
+      });
     });
-    xit('returns an error when a name is not provided', () => {
-
+    it('returns an error when a name is not provided', (done) => {
+      chai.request(app)
+        .post('/projects')
+        .send({ slug: 'test' })
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.should.be.json;
+          res.body.status.should.equal('error');
+          res.body.message.should.be.equal('Name is required.');
+          done();
+        });
     });
-    xit('returns an error when a slug is not provided', () => {
-
+    it('returns an error when a slug is not provided', (done) => {
+      chai.request(app)
+        .post('/projects')
+        .send({ name: 'Test' })
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.should.be.json;
+          res.body.status.should.equal('error');
+          res.body.message.should.be.equal('Slug is required.');
+          done();
+        });
     });
-    xit('returns an error when an invalid slug is provided (punctuation)', () => {
-
+    it('returns an error when an invalid slug is provided (punctuation)', (done) => {
+      chai.request(app)
+        .post('/projects')
+        .send({ name: 'Test', slug: 'test!' })
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.should.be.json;
+          res.body.status.should.equal('error');
+          res.body.message.should.be.equal('Slug is invalid.');
+          done();
+        });
     });
-    xit('returns an error when an invalid slug is provided (trailing-slash)', () => {
-
+    it('returns an error when an invalid slug is provided (trailing-slash)', (done) => {
+      chai.request(app)
+        .post('/projects')
+        .send({ name: 'Test', slug: 'test-' })
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.should.be.json;
+          res.body.status.should.equal('error');
+          res.body.message.should.be.equal('Slug is invalid.');
+          done();
+        });
     });
-    xit('returns an error when a duplicate slug is provided', () => {
-
+    it('returns an error when a duplicate slug is provided', (done) => {
+      const project = new Project({
+        name: 'Test 1',
+        slug: 'test'
+      });
+      project.save().then(() => {
+        chai.request(app)
+          .post('/projects')
+          .send({ name: 'Test 2', slug: 'test' })
+          .end((err, res) => {
+            res.should.have.status(409);
+            res.should.be.json;
+            res.body.status.should.equal('error')
+            res.body.message.should.be.equal('Slug is in use.');
+            done();
+          });
+      });
     });
-    xit('converts a slug to lowercase', () => {
-
+    it('converts a slug to lowercase', (done) => {
+      chai.request(app)
+        .post('/projects')
+        .send({ name: 'TEST', slug: 'TEST' })
+        .end((err, res) => {
+          res.should.be.json;
+          res.body.status.should.equal('success')
+          res.body.data.slug.should.be.equal('test');
+          done();
+        });
     });
-    xit('automatically assigns a creation date', () => {
-
+    it('automatically assigns a creation date', (done) => {
+      chai.request(app)
+        .post('/projects')
+        .send({ name: 'Test', slug: 'test' })
+        .end((err, res) => {
+          res.should.be.json;
+          res.body.status.should.equal('success')
+          res.body.data._createdAt.should.not.be.null;
+          done();
+        });
     });
   })
 
@@ -83,47 +165,47 @@ describe('Projects API', () => {
   //  /projects/:id
   //////////////////////////////////////////////////////////
 
-  describe('GET /projects/:id', () => {
-    xit('lists a SINGLE project', () => {
+  describe('GET /projects/:id', (done) => {
+    xit('lists a SINGLE project', (done) => {
 
     })
-    xit('returns a 404 status for invalid ids', () => {
+    xit('returns a 404 status for invalid ids', (done) => {
 
     })
-    xit('returns a 404 status for non-existent ids', () => {
-
-    })
-  })
-
-  describe('PUT /projects/:id', () => {
-    xit('updates a SINGLE project', () => {
-
-    });
-    xit('only updates provided fields', () => {
-
-    })
-    xit('does not modify the original id', () => {
-
-    })
-    xit('returns error status for invalid ids', () => {
-
-    })
-    xit('returns error status for non-existent ids', () => {
-
-    })
-    xit('returns an error when an invalid slug is provided', () => {
+    xit('returns a 404 status for non-existent ids', (done) => {
 
     })
   })
 
-  describe('DELETE /projects/:id', () => {
-    xit('deletes a SINGLE project', () => {
+  describe('PUT /projects/:id', (done) => {
+    xit('updates a SINGLE project', (done) => {
 
     });
-    xit('returns a 404 status for invalid ids', () => {
+    xit('only updates provided fields', (done) => {
+
+    })
+    xit('does not modify the original id', (done) => {
+
+    })
+    xit('returns error status for invalid ids', (done) => {
+
+    })
+    xit('returns error status for non-existent ids', (done) => {
+
+    })
+    xit('returns an error when an invalid slug is provided', (done) => {
+
+    })
+  })
+
+  describe('DELETE /projects/:id', (done) => {
+    xit('deletes a SINGLE project', (done) => {
 
     });
-    xit('returns a 404 status for non-existent ids', () => {
+    xit('returns a 404 status for invalid ids', (done) => {
+
+    });
+    xit('returns a 404 status for non-existent ids', (done) => {
 
     });
   })
