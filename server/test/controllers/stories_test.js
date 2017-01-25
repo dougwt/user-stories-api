a941e6a68e79765b345d8c07ab1237cdb369a2f0
Doug Tyler <dougwt@gmail.com>
2017-01-24 16:02:32 -0800

    Add POST /projects/:id/roles/:id/stories

--- server/test/controllers/stories_test.js
+++ server/test/controllers/stories_test.js
@@ -13,27 +13,131 @@ describe('Stories API', () => {
   //  /projects/:id/stories
   //////////////////////////////////////////////////////////
 
-  describe('GET /projects/:id/stories', () => {
-    xit('lists ALL stories', () => {
-
+  describe('GET /projects/:id/roles/:id/stories', () => {
+    it('lists ALL stories', (done) => {
+      const p1 = new Project({
+        name: 'Test Project',
+        slug: 'test-project',
+        roles: [{
+          name: 'Tester',
+          stories: [{
+            desire: 'find errors',
+            benefit: 'they can be fixed'
+          }]
+        }]
+      });
+      p1.save().then(() => {
+        chai.request(app)
+          .get(`/projects/${p1._id}/roles/${p1.roles[0]._id}/stories`)
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.should.be.json;
+            res.body.status.should.equal('success')
+            res.body.data.should.be.a('array');
+            res.body.data.length.should.be.gte(1);
+            res.body.data[0].should.have.property('benefit');
+            res.body.data[0].desire.should.equal('find errors')
+            res.body.data[0].should.have.property('desire');
+            res.body.data[0].benefit.should.equal('they can be fixed')
+            res.body.data[0].should.have.property('_createdAt');
+            done();
+          })
+      });
     });
-    xit('returns an empty list when the collection is empty', () => {
-
+    it('returns an empty list when the collection is empty', (done) => {
+      const p1 = new Project({
+        name: 'Test Project',
+        slug: 'test-project',
+        roles: [{
+          name: 'Tester',
+          stories: []
+        }]
+      });
+      p1.save().then(() => {
+        chai.request(app)
+          .get(`/projects/${p1._id}/roles/${p1.roles[0]._id}/stories`)
+          .end((err, res) => {
+            p1.roles[0].stories.length.should.be.equal(0)
+            res.should.have.status(200);
+            res.should.be.json;
+            res.body.status.should.equal('success')
+            res.body.data.should.be.a('array');
+            res.body.data.length.should.equal(0);
+            done();
+          })
+      });
     });
   })
 
-  describe('POST /projects/:id/stories', () => {
-    xit('creates a new story', () => {
+  describe('POST /projects/:id/roles/:id/stories', () => {
+    let p1;
 
+    beforeEach((done) => {
+      p1 = new Project({
+        name: 'Test Project',
+        slug: 'test-project',
+        roles: [{
+          name: 'Tester',
+          stories: []
+        }]
+      });
+      p1.save().then(() => {
+        done()
+      })
+    })
+
+    it('creates a new story', (done) => {
+      const count = p1.roles[0].stories.length;
+      chai.request(app)
+        .post(`/projects/${p1._id}/roles/${p1.roles[0]._id}/stories`)
+        .send({ desire: 'find errors', benefit: 'they can be fixed' })
+        .end((err, res) => {
+          Project.findById(p1.id).then(project => {
+            const newCount = project.roles[0].stories.length
+            res.should.have.status(201);
+            res.should.be.json;
+            res.headers.should.have.property('location');
+            res.headers.location.startsWith(`https://api.mycodebytes.com/v1/projects/${project._id}/roles`);
+            res.body.status.should.equal('success');
+            newCount.should.equal(count + 1);
+            done();
+          });
+        });
     });
-    xit('returns an error when a desire is not provided', () => {
-
+    it('returns an error when a desire is not provided', (done) => {
+      chai.request(app)
+        .post(`/projects/${p1._id}/roles/${p1.roles[0]._id}/stories`)
+        .send({ benefit: 'they can be fixed' })
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.should.be.json;
+          res.body.status.should.equal('error');
+          res.body.message.should.be.equal('Desire is required.');
+          done();
+        });
     });
-    xit('returns an error when a benefit is not provided', () => {
-
+    it('returns an error when a benefit is not provided', (done) => {
+      chai.request(app)
+        .post(`/projects/${p1._id}/roles/${p1.roles[0]._id}/stories`)
+        .send({ desire: 'find errors' })
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.should.be.json;
+          res.body.status.should.equal('error');
+          res.body.message.should.be.equal('Benefit is required.');
+          done();
+        });
     });
-    xit('automatically assigns a creation_date', () => {
-
+    it('automatically assigns a creation_date', (done) => {
+      chai.request(app)
+        .post(`/projects/${p1._id}/roles/${p1.roles[0]._id}/stories`)
+        .send({ desire: 'find errors', benefit: 'they can be fixed' })
+        .end((err, res) => {
+          res.should.be.json;
+          res.body.status.should.equal('success')
+          res.body.data[0]._createdAt.should.not.be.null;
+          done();
+        });
     });
   })
 
@@ -41,44 +145,44 @@ describe('Stories API', () => {
   //  /projects/:id/stories/:id
   //////////////////////////////////////////////////////////
 
-  describe('GET /projects/:id/stories/:id', () => {
-    xit('lists a SINGLE story', () => {
+  describe('GET /projects/:id/roles/:id/stories/:id', () => {
+    xit('lists a SINGLE story', (done) => {
 
     })
-    xit('returns a 404 status for invalid ids', () => {
+    xit('returns a 404 status for invalid ids', (done) => {
 
     });
-    xit('returns a 404 status for non-existent ids', () => {
-
-    });
-  })
-
-  describe('PUT /projects/:id/stories/:id', () => {
-    xit('updates a SINGLE story', () => {
-
-    });
-    xit('only updates provided fields', () => {
-
-    });
-    xit('does not modify the original id', () => {
-
-    });
-    xit('returns an error for invalid ids', () => {
-
-    });
-    xit('returns an error for non-existent ids', () => {
+    xit('returns a 404 status for non-existent ids', (done) => {
 
     });
   })
 
-  describe('DELETE /projects/:id/stories/:id', () => {
-    xit('deletes a SINGLE story', () => {
+  describe('PUT /projects/:id/roles/:id/stories/:id', () => {
+    xit('updates a SINGLE story', (done) => {
 
     });
-    xit('returns an error for invalid ids', () => {
+    xit('only updates provided fields', (done) => {
 
     });
-    xit('returns an error for non-existent ids', () => {
+    xit('does not modify the original id', (done) => {
+
+    });
+    xit('returns an error for invalid ids', (done) => {
+
+    });
+    xit('returns an error for non-existent ids', (done) => {
+
+    });
+  })
+
+  describe('DELETE /projects/:id/roles/:id/stories/:id', () => {
+    xit('deletes a SINGLE story', (done) => {
+
+    });
+    xit('returns an error for invalid ids', (done) => {
+
+    });
+    xit('returns an error for non-existent ids', (done) => {
 
     });
   })
