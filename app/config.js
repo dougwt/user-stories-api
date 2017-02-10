922d4d5ae9c0a98d4950c5ee59bd6831f8501569
Doug Tyler <dougwt@gmail.com>
2017-02-10 15:17:51 -0800

    Add API_PORT to app/config.js

--- app/config.js
+++ app/config.js
@@ -2,6 +2,8 @@
 // Used to determine which environment the app is currently running in.
 export const NODE_ENV = process.env.NODE_ENV || 'dev'
 
+export const API_PORT = process.env.API_PORT || 8080;
+
 // MongoDB Connection String URI (excluding the 'mongodb://' prefix)
 // Reference: https://docs.mongodb.com/manual/reference/connection-string/
 export const MONGODB_CONNECTION = process.env.MONGODB_CONNECTION || 'localhost/user_stories';
