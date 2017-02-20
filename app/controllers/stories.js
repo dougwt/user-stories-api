02b06710d455d3130306852d60e2ade5c01330c6
Doug Tyler <dougwt@gmail.com>
2017-02-20 15:37:23 -0800

    Clean up controllers/stories

--- app/controllers/stories.js
+++ app/controllers/stories.js
@@ -1,6 +1,7 @@
 import Project from '../models/project';
 import Response from '../response'
 
+// TODO: refactor to utils
 function renameNestedProps(roleProps, prefix) {
   // Source: http://stackoverflow.com/a/32452554
   function fix_key(key) { return prefix + key; }
@@ -12,36 +13,46 @@ function renameNestedProps(roleProps, prefix) {
 
 module.exports = {
 
+  // Find all project stories [GET /projects/:projectId/stories]
   findAll(req, res, next) {
     const projectId = req.params.projectId;
 
+    // TODO: refactor to utils
     const sortFunc = function(a, b) {
       return b._createdAt - a._createdAt;
     };
 
     Project.findById(projectId)
       .then((project) => {
-            if (project) {
-              const beginSlice = req.query.skip ? parseInt(req.query.skip) : 0;
-              const limit = req.query.limit ? parseInt(req.query.limit) + beginSlice : project.stories.length
-              const endSlice = Math.min(limit, 100);
-              const result = project.stories.sort(sortFunc).slice(beginSlice, endSlice);
-              res.status(200).send(Response.success(result))
-            } else {
-              var err = new Error();
-              err.status = 404;
-              next(err);
-            }
+        // If the requested project was found...
+        if (project) {
+          // Determine which slice of the project's stories to return
+          const beginSlice = req.query.skip ? parseInt(req.query.skip) : 0;
+          const limit = req.query.limit ? parseInt(req.query.limit) + beginSlice : project.stories.length
+          const endSlice = Math.min(limit, 100);
+          const result = project.stories.sort(sortFunc).slice(beginSlice, endSlice);
+          // Return the results along with a success response.
+          res.status(200).send(Response.success(result))
+        } else {
+          // Otherwise, send a Not Found error response.
+          var err = new Error();
+          err.status = 404;
+          next(err);
         }
-      )
+      })
+      .catch((err) => {
+        return next(err)
+      });
   },
 
+  // Create a new project story [POST /projects/:projectId/stories]
   create(req, res, next) {
     const projectId = req.params.projectId;
     const roleId = req.params.roleId;
+    // Pull off only the specific props we expect the form to submit
+    // and build our own props obj to protect from malicious clients.
     const { _id, id, role, desire, benefit, author } = req.body;
     const project = req['requestedProject']
-
     const storyProps = {};
     if (_id || id) { return res.status(403).send(Response.error('This action is forbidden.')); }
     if (role) { storyProps['role'] = role };
@@ -49,6 +60,7 @@ module.exports = {
     if (benefit) { storyProps['benefit'] = benefit };
     if (author) { storyProps['author'] = author };
 
+    // Create the new project story with our sanitized input.
     project.stories.push(storyProps);
     project.save((err) => {
       if (err) {
@@ -69,12 +81,14 @@ module.exports = {
     });
   },
 
+  // Update an existing project story [PUT /projects/:projectId/stories/:storyId]
   update(req, res, next) {
     const projectId = req.params.projectId;
     const roleId = req.params.roleId;
     const storyId = req.params.storyId;
+    // Pull off only the specific props we expect the form to submit
+    // and build our own props obj to protect from malicious clients.
     const { _id, id, role, desire, benefit, author } = req.body;
-
     const storyProps = {};
     if (_id || id) { return res.status(403).send(Response.error('This action is forbidden.')); }
     if (role) { storyProps['role'] = role };
@@ -82,18 +96,23 @@ module.exports = {
     if (benefit) { storyProps['benefit'] = benefit };
     if (author) { storyProps['author'] = author };
 
+    // Rename each prop to account for MongoDB's nested document structure.
     const renamedProps = renameNestedProps(storyProps, 'stories.$.')
 
+    // Update the project with our sanitized input.
     Project.findOneAndUpdate(
       { "_id": projectId, "stories._id": storyId },
       { $set: renamedProps },
       { runValidators: true, context: 'query' }
     )
       .then((project) => {
+        // If the requested project was found...
         if (project) {
+          // Return the project along with a success response.
           // TODO: replace hardcoded URI prefix
           return res.location('https://api.mycodebytes.com/v1/projects/'+ project._id).status(204).send(Response.success(project))
         }
+        // Otherwise, send a Not Found error response.
         var err = new Error();
         err.status = 404;
         next(err);
@@ -103,23 +122,26 @@ module.exports = {
       });
   },
 
+  // Delete an existing project story [DELETE /projects/:projectId/stories/:storyId]
   delete(req, res, next) {
     const projectId = req.params.projectId;
     const storyId = req.params.storyId;
     const project = req['requestedProject']
     const story = req['requestedStory']
 
+    // If the requested role was not found...
     if (story === null) {
+      // Return a Not Found error response.
       var err = new Error();
       err.status = 404;
       next(err);
     }
 
+    // Otherwise, attempt to delete the role.
     story.remove();
     project.save((err) => {
-      if (err) {
-        next(err);
-      }
+      if (err) { next(err); }
+      // Send a success response with the project.
       return res.status(204).send(Response.success(project))
     });
   }
